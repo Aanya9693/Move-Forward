a7d78b6611fb8a9803e111f2e5ee4f46d67a1d6f
Aanya <Aanyasah9@gmail.com>
2023-06-02 22:04:39 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Searchbox from './components/Searchbox/Searchbox'
+import Jobs from './components/opportunities/opportunities'
+import Footer from './components/Footer/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    
+    <div className='w-[85%] m-auto bg-white'>
+      <Navbar />
+      <Searchbox />
+      <Jobs />
+      <Footer />
+    </div>
   )
 }
 
