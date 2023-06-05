cd90d2a804689e4ef24439768eb3fc26a48b6aed
Aanya <Aanyasah9@gmail.com>
2023-06-05 20:35:14 +0530

    added the table

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,49 +1,10 @@
 export const List = [
-    // Start of Jan
     
     {
       name: 'Qualcomm Global Scholars Program',
       link: 'https://www.iie.org/Programs/WeTech/STEM-Scholarships-for-Women/Qualcomm-Global-Scholars-Program/India',
       deadline: '2 January',
       type: 'Scholarship for Women',
-    },
-    {
-      name: 'Goldman Sachs Engineering Campus Hiring Program',
-      link: 'https://www.goldmansachs.com/careers/students/programs/india/engineers-campus-hiring-program.html',
-      deadline: '3 January 2022',
-      type: 'Internship and FTE',
-    },
-    
-    
-    {
-      name: 'Season of KDE',
-      link: 'https://season.kde.org/',
-      deadline: '4 January',
-      type: 'Summer of Code/ Mentorship',
-    },
-    {
-      name: 'Microsoft Research Fellows Program',
-      link: 'https://www.microsoft.com/en-us/research/academic-program/research-fellows-program-at-microsoft-research-india/',
-      deadline: '14 January 2022',
-      type: 'Fellowship',
-    },
-    {
-      name: 'Goldman Sachs Women Possibilities Summit: Finance',
-      link: 'https://www.goldmansachs.com/careers/students/programs/india/womens-possibilities-summit-finance.html',
-      deadline: '15 January',
-      type: 'Scholarship to attend conference',
-    },
-    {
-      name: 'Internshala Career Scholarship for Girls',
-      link: 'https://blog.internshala.com/2021/12/internshala-career-scholarship-for-girls-2022/',
-      deadline: '15 January 2022',
-      type: 'Scholarship',
-    },
-    {
-      name: 'ETHWMN Fellowship',
-      link: 'https://ethwmn.devfolio.co/',
-      deadline: '15 January 2022',
-      type: 'Fellowship for women',
     }
 ]
 
