98cae430471e77c71dfe42b21d77889d79b46050
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-06-12 14:48:31 +0530

    [BUILD]: added pagination for smooth rendering

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, {useState, useMemo} from "react";
 import "./Table.css";
 import Tag from "../Tag/Tag";
-import data from "../../assets/dev_data/testData.json";
+// import data from "../../assets/dev_data/testData.json";
+import data from "../../assets/dev_data/data4.json";
 import Button from "../Button/Button";
 import SearchResults from "./SearchResults";
+import Pagination from "../Pagination/Pagination";
 
 const Table = ({
 	tags,
@@ -17,16 +19,30 @@ const Table = ({
 
 	const pageLength = 10;
 
-	
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const currentTableData = useMemo(() => {
+		const firstPageIndex = (currentPage - 1) * pageLength;
+		const lastPageIndex = firstPageIndex + pageLength;
+		return data.slice(firstPageIndex, lastPageIndex);
+	}, [currentPage]);
+
 	return (
 		<div className="table">
 			<div className="divider"></div>
 			<SearchResults
-				data={data}
-				selectedSuperTags= {selectedSuperTags}
-				selectedTags= {selectedTags}
-				handleTagDeselect= {handleTagDeselect}
-				handleTagSelect= {handleTagSelect}
+				data={currentTableData}
+				selectedSuperTags={selectedSuperTags}
+				selectedTags={selectedTags}
+				handleTagDeselect={handleTagDeselect}
+				handleTagSelect={handleTagSelect}
+			/>
+			<Pagination
+				className="pagination-bar"
+				currentPage={currentPage}
+				totalCount={data.length}
+				pageSize={pageLength}
+				onPageChange={(page) => setCurrentPage(page)}
 			/>
 		</div>
 	);
