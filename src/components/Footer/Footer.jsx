d065194dd69bae7208ec328aa14134e7ab0276d9
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-06-15 21:25:57 +0530

    [BUILD]: updated design and added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
+		<div className="footer">
+			Made with 💚 by
+			<a href="https://dugarrishab.github.io/RishabDugar/"> Rishab </a>
+      and 
+      <a href=""> Aana</a>
+		</div>
+  );
 }
 
 export default Footer
--- src/components/Table/SearchResults.jsx
+++ src/components/Table/SearchResults.jsx
@@ -11,79 +11,99 @@ const SearchResults = ({
 	handleTagSelect,
 	data,
 }) => {
-	return data.length === 0 ? ("No results Found") :  data.map((item) => (
-		<div className="item" key={item.name}>
-			<div className="row-1">
-				<div className="col-1">
-					<div className="logo"></div>
-				</div>
-				<div className="col-2">
-					<div className="title">{item.name}</div>
-					<div className="org">{item.org}</div>
-				</div>
-				<div className="col-3">
-					{/* <span className="material-icons">notifications_none</span> */}
-					<span className="material-icons">bookmark_border</span>
-				</div>
-			</div>
-			<div className="row-2 description">{item.description}</div>
-			<div className="row-3 tags">
-				{
-					<Tag
-						innerHtml={item.type}
-						key={item.type}
-						color="green"
-						type="secondary"
-						onClick={
-							selectedSuperTags.includes(item.type)
-								? () => handleTagDeselect(item.type, true)
-								: () => handleTagSelect(item.type, true)
-						}
-						select={selectedSuperTags.includes(item.type)}
-					></Tag>
-				}
-				{item.tags.map((tag) => (
-					<Tag
-						innerHtml={tag}
-						key={tag}
-						color="yellow"
-						type="secondary"
-						onClick={
-							selectedTags.includes(tag)
-								? () => handleTagDeselect(tag, false)
-								: () => handleTagSelect(tag, false)
-						}
-						select={selectedTags.includes(tag)}
-					></Tag>
-				))}
-			</div>
-			<div className="divider"></div>
-			<div className="row-4 details">
-				<div className="col-1">
-					<div className="date">
-						<div className="icon">
-							<span className="material-icons">event</span>
-						</div>
-						{item.deadline}
+	return data.length === 0
+		? "No results Found"
+		: data.map((item) => (
+			<div className="item" key={item.name}>
+				<div className="row-1">
+					<div className="col-1">
+						<div className="logo"></div>
 					</div>
-					<div className="stipend">
-						<div className="icon">
-							<span className="material-icons">payments</span>
-						</div>
-						{item.stipend}
+					<div className="col-2">
+						<div className="title">{item.name}</div>
+						<div className="org">{item.org}</div>
+					</div>
+					<div className="col-3">
+						{/* <span className="material-icons">notifications_none</span> */}
+						<span className="material-icons">
+								bookmark_border
+						</span>
 					</div>
 				</div>
-				<div className="col-2">
-					{/* <Button
+				<div className="row-2 description">{item.description}</div>
+				<div className="row-3 tags">
+					{
+						<Tag
+							innerHtml={item.type}
+							key={item.type}
+							color="green"
+							type="secondary"
+							onClick={
+								selectedSuperTags.includes(item.type)
+									? () =>
+										handleTagDeselect(
+											item.type,
+											true
+										)
+									: () => handleTagSelect(item.type, true)
+							}
+							select={selectedSuperTags.includes(item.type)}
+						></Tag>
+					}
+					{item.tags.map((tag) => (
+						<Tag
+							innerHtml={tag}
+							key={tag}
+							color="yellow"
+							type="secondary"
+							onClick={
+								selectedTags.includes(tag)
+									? () => handleTagDeselect(tag, false)
+									: () => handleTagSelect(tag, false)
+							}
+							select={selectedTags.includes(tag)}
+						></Tag>
+					))}
+				</div>
+				<div className="divider"></div>
+				<div className="row-4 details">
+					<div className="col-1">
+						<div className="date">
+							<div className="icon">
+								<span className="material-icons">
+										event
+								</span>
+							</div>
+							{item.deadline}
+						</div>
+						<div className="stipend">
+							<div className="icon">
+								<span className="material-icons">
+										payments
+								</span>
+							</div>
+							{item.stipend}
+						</div>
+						<div className="country">
+							<div className="icon">
+								<span className="material-icons">
+										language
+								</span>
+							</div>
+							{item.country}
+						</div>
+					</div>
+					<div className="col-2">
+						{/* <Button
 						innerText="Share"
 						variant="primary"
 						color="green"
 						endIcon={<span className="material-icons">share</span>}
 					/> */}
+					</div>
 				</div>
 			</div>
-		</div>
-	));
+		  ));
 };
 
 export default SearchResults;
